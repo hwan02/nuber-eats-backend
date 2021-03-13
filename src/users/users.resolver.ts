6a0ce1869ee248b5a9d23a57f831739c1cf355ee
Daniel <ssh32009757@gmail.com>
2021-03-13 18:01:10 +0900

    User profile done

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -3,7 +3,9 @@ import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AuthUser } from "src/auth/auth-user.decorator";
 import { AuthGuard } from "src/auth/auth.guard";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
+import { EditProfileInput, EditProfileOutput } from "./dtos/edit-profile.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -47,5 +49,44 @@ export class UsesrsResolver {
     @UseGuards(AuthGuard)
     me(@AuthUser() authUser: User) {
         return authUser;
-      }
+    }
+
+    @UseGuards(AuthGuard)
+    @Query(returns => UserProfileOutput)
+    async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+        try {
+            const user = await this.usersService.findById(userProfileInput.userId);
+            if (!user) {
+                throw Error();
+            }
+            return {
+                ok: true,
+                user,
+            };
+        } catch (e) {
+            return {
+                error: 'User Not Found',
+                ok: false,
+            };
+        }
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation(returns => EditProfileOutput)
+    async editProfile(
+        @AuthUser() authUser: User,
+        @Args('input') editProfileInput: EditProfileInput,
+    ): Promise<EditProfileOutput> {
+        try {
+            await this.usersService.editProfile(authUser.id, editProfileInput);
+            return {
+                ok: true
+            }
+        } catch (error) {
+            return {
+                ok: false,
+                error,
+            };
+        }
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,7 @@ import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { JwtService } from "src/jwt/jwt.service";
+import { EditProfileInput } from "./dtos/edit-profile.dto";
 
 @Injectable()
 export class UsersService{
@@ -54,5 +55,16 @@ export class UsersService{
       }
       async findById(id: number): Promise<User> {
         return this.users.findOne({ id });
+      }
+
+      async editProfile(userId: number, {email, password}: EditProfileInput): Promise<User> {
+        const user = await this.users.findOne(userId);
+        if(email){
+          user.email = email
+        }
+        if(password){
+          user.password = password
+        }
+        return this.users.save(user);
       }
 }
