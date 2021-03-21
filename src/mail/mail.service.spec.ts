4196d6cebe3b39bbc7ad9e1714d1e708877f9ea0
Daniel <ssh32009757@gmail.com>
2021-03-21 17:02:23 +0900

    Unit Test Done

---
+++ src/mail/mail.service.spec.ts
@@ -0,0 +1,81 @@
+// jest는 자동으로 describe, beforEach를 import한다.
+import { Test } from '@nestjs/testing';
+import got from 'got';
+import * as FormData from 'form-data';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailService } from './mail.service';
+
+jest.mock('got');
+jest.mock('form-data');
+
+const TEST_DOMAIN = 'test-domain';
+
+describe('MailService', () => {
+    let service: MailService;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                MailService,
+                {
+                    provide: CONFIG_OPTIONS,
+                    useValue: {
+                        apiKey: 'test-apiKey',
+                        domain: TEST_DOMAIN,
+                        fromEmail: 'test-fromEmail',
+                    },
+                },
+            ],
+        }).compile();
+        service = module.get<MailService>(MailService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+    describe('sendVerificationEmail', () => {
+        it('should call sendEmail', () => {
+            const sendVerificationEmailArgs = {
+                email: 'email',
+                code: 'code',
+            };
+            jest.spyOn(service, 'sendEmail').mockImplementation(async () => true);
+            service.sendVerificationEmail(
+                sendVerificationEmailArgs.email,
+                sendVerificationEmailArgs.code,
+            );
+            expect(service.sendEmail).toHaveBeenCalledTimes(1);
+            expect(service.sendEmail).toHaveBeenCalledWith(
+                'Verify Your Email',
+                'verify-email',
+                [
+                    { key: 'code', value: sendVerificationEmailArgs.code },
+                    { key: 'username', value: sendVerificationEmailArgs.email },
+                ],
+            );
+        });
+    });
+    describe('sendEmail', () => {
+        it('sends email', async () => {
+          const ok = await service.sendEmail('', '', [
+            { key: 'attr', value: 'attrValue' },
+            ]);
+          const formSpy = jest.spyOn(FormData.prototype, 'append');
+          expect(formSpy).toHaveBeenCalledTimes(5);
+          expect(formSpy).toHaveBeenCalled();
+          expect(got.post).toHaveBeenCalledTimes(1);
+          expect(got.post).toHaveBeenCalledWith(
+            `https://api.mailgun.net/v3/${TEST_DOMAIN}/messages`,
+            expect.any(Object),
+          );
+          expect(ok).toEqual(true);
+        });
+        it('fails on error', async () => {
+          jest.spyOn(got, 'post').mockImplementation(() => {
+            throw new Error();
+          });
+          const ok = await service.sendEmail('', '', []);
+          expect(ok).toEqual(false);
+        });
+      });
+});
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,13 +8,13 @@ import { EmailVar, MailModuleOptions } from './mail.interfaces';
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {}
+  ) { }
 
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     emailVars: EmailVar[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append(
       'from',
@@ -25,17 +25,20 @@ export class MailService {
     form.append('template', template);
     emailVars.forEach(eVar => form.append(`v:${eVar.key}`, eVar.value));
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+      await got.post(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      });
+      );
+      return true;
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
